28bedcf001f9478032a64bca56ab8ba5e9fb6c83
pascal <se@dd.io>
2021-09-12 21:47:40 +0800

    1st

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 import requests
-# headers = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
-#     }
-# data = {
-#     'my_username':'ldsm3392',
-#     'my_password':'ld65413392',
-# }
-# url='https://passport.mysteel.com/loginJson.jsp?callback=loginJsn&my_username=ldsm3392&my_password=c0a847d46f58d3a75c77e828be1e2c111&callbackJsonp=loginJsn&jumpPage=https%3A%2F%2Fwww.mysteel.com%2F&site=www.mysteel.com&my_rememberStatus=true&vcode=&_=1631176869695'
-# session=requests.Session()
-# session.post(url,headers=headers)
-#
-# r=session.get('https://jiancai.mysteel.com/m/21090910/D6D83293AE0C2195.html')
-# r=r.text
-# print(r.status_code)
-# print(r.text)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+}
+data = {
+    'my_username': 'ldsm3392',
+    'my_password': 'ld65413392',
+}
+url = 'https://passport.mysteel.com/loginJson.jsp?callback=loginJsn&my_username=ldsm3392&my_password' \
+      '=c0a847d46f58d3a75c77e828be1e2c111&callbackJsonp=loginJsn&jumpPage=https%3A%2F%2Fwww.mysteel.com%2F&site=www' \
+      '.mysteel.com&my_rememberStatus=true&vcode=&_=1631176869695 '
+session = requests.Session()
+session.post(url, headers=headers)
+
+r = session.get('https://jiancai.mysteel.com/m/21090915/FE60DE688CB0E86D.html')
+r = r.text
+print(r)
 
 from lxml import etree
-f=open('./mysteel.html','r',encoding='utf-8')
-r=f.read()
 
-page=etree.HTML(r)
-print(page)
-print(page.xpath('head'))
-print(page.xpath('//h1/text()'))
-print(page.xpath('//tr[@id="ctr1"]/td/text()'))
+# f = open('./mysteel.html', 'r', encoding='utf-8')
+# r = f.read()
+
+page = etree.HTML(r)
+t = page.xpath('//h1/text()')
+t = str(t)
+t = t[2:len(t) - 2]
+print(t)
+l1 = page.xpath('//tr[contains(@id,"ctr")]/td/text()')
+l = [elem.strip() for elem in l1]
+for i in range(0, len(l), 8):
+    print(l[i:i + 8])
 
 import xlwt
-f=xlwt.Workbook()
-sheet1=f.add_sheet(u'钢材价格')
-rowTitle=[u'品名',u'规格(mm)',u'材质',u'钢厂/产地',u'价格(元/吨)',u'涨跌',u'备注',u'钢号']
-for i in range(0,len(rowTitle)):
-    sheet1.write(0,i,rowTitle[i])
-f.save('./mysteel.xlsx')
+
+f = xlwt.Workbook()
+sheet1 = f.add_sheet(u'钢材价格')
+sheet1.write(0, 0, t)
+rowTitle = [u'品名', u'规格(mm)', u'材质', u'钢厂/产地', u'价格(元/吨)', u'涨跌', u'备注', u'钢号']
+for i in range(0, len(rowTitle)):
+    sheet1.write(1, i, rowTitle[i])
+k = 0
+row = int(len(l) / 8)
+for j in range(2, row):
+    for i in range(0, 8):
+        sheet1.write(j, i, l[k])
+        k = k + 1
+
+print(t)
+f.save('./' + t + '.xlsx')
---
+++ s2.py
@@ -0,0 +1,35 @@
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+}
+data = {
+    'my_username': 'ldsm3392',
+    'my_password': 'ld65413392',
+}
+url = 'https://passport.mysteel.com/loginJson.jsp?callback=loginJsn&my_username=ldsm3392&my_password' \
+      '=c0a847d46f58d3a75c77e828be1e2c111&callbackJsonp=loginJsn&jumpPage=https%3A%2F%2Fwww.mysteel.com%2F&site=www' \
+      '.mysteel.com&my_rememberStatus=true&vcode=&_=1631176869695 '
+session = requests.Session()
+session.post(url, headers=headers)
+
+r = session.get('https://chongqing.mysteel.com/')
+print(r.encoding)
+r = r.text
+r.encode('iso-8859-1').decode('iso-8859-1')
+
+import chardet
+import urllib
+
+raw_data = urllib.urlopen('https://chongqing.mysteel.com/').read()
+print(chardet.detect(raw_data))
+
+from lxml import etree
+
+page = etree.HTML(r)
+t = page.xpath('//h2/text()')
+t = str(t)
+print(t)
+# f = open('./mysteel2.html', 'r', encoding='utf-8')
+# r = f.read()
+# print(r)
